54a1e4e90b54c82e46849c88b1437c5c4714ddbb
Pawarshekhar709 <pawarshekhar708@gmail.com>
2025-08-14 16:13:43 +0530

    commit message

--- ApnaHrms/settings.py
+++ ApnaHrms/settings.py
@@ -123,7 +123,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = "/ApnaHrms/static/"
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "ApnaHrms/static",
