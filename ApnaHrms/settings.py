b0e30f2421d1b23beb5322a84146f512f6c8ff89
Praphull Subhash Sangle <sangalepraphulla9689@gmail.com>
2025-09-02 15:44:59 +0530

    ui changes

--- ApnaHrms/settings.py
+++ ApnaHrms/settings.py
@@ -122,12 +122,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# STATIC_URL = "/ApnaHrms/static/"
-STATIC_URL = "/static/"
+STATIC_URL = "/ApnaHrms/static/"
+# STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    # BASE_DIR / "ApnaHrms/static",
-    BASE_DIR / "static",
+    BASE_DIR / "ApnaHrms/static",
+    # BASE_DIR / "static",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
